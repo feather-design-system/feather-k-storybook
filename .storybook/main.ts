c45d40c9c615f65c06bab91bd69c578eefe247e3
janderson_nha <janderson@nanthealth.com>
2024-04-23 15:13:02 -0400

    feat(upgrade): storybook to 8.0.9

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -2,20 +2,27 @@ import type { StorybookConfig } from "@storybook/vue3-vite";
 
 const config: StorybookConfig = {
   stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
+
   addons: [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
     "@chromatic-com/storybook",
     "@storybook/addon-interactions",
+    "@storybook/addon-mdx-gfm"
   ],
+
+  // docs: {
+  //   autodocs: "tag",
+  // },
   framework: {
     name: "@storybook/vue3-vite",
     options: {
       docgen: 'vue-component-meta',
     },
   },
-  // docs: {
-  //   autodocs: "tag",
-  // },
+
+  docs: {
+    autodocs: true
+  }
 };
 export default config;
