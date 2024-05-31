2fc63f7e139364884ea54eda59da5ef923a9bb41
janderson_nha <janderson@nanthealth.com>
2024-05-31 12:04:57 -0400

    chore: responsive grid demo; containers.

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
 import { Button } from "@progress/kendo-vue-buttons";
+import { ComboBox } from "@progress/kendo-vue-dropdowns";
 import {
   Card,
   CardHeader,
@@ -9,16 +10,47 @@ import {
   CardFooter,
   CardTitle,
 } from "@progress/kendo-vue-layout";
+import { TextBox } from "@progress/kendo-vue-inputs";
 import "./assets/css/feather-ks.css";
 import "./assets/css/feather-k-override.css";
 </script>
 
 <template>
+
+  
   <header class="feather-k-container-page-title">
-    <h2>Page Title</h2>
+    <h3>Page Title</h3>
+    <span>Some Text</span>
   </header>
+
+  
+  <div class="feather-k-container-standard">
+    <TextBox 
+      class="patient-search"
+      label="Patient Search"
+      placeholder="Enter patient name" 
+      fillMode="outline" 
+      rounded="medium" 
+      size="medium" 
+    />
+  </div>
   <div class="feather-k-container-standard">
     <div class="buttons">
+      <TextBox 
+      label="Patient Search"
+      placeholder="Search for patient" 
+      fillMode="outline" 
+      rounded="medium" 
+      size="medium" 
+    />
+
+      <ComboBox 
+        :dataItems="['Baseball', 'Basketball', 'Football', 'Golf', 'Tennis']" 
+        label="Favorite Sport"
+        fillMode="outline" 
+        rounded="medium" 
+        size="medium" 
+      />
       <Button
         type="submit"
         fillMode="solid"
@@ -86,20 +118,16 @@ body {
   margin: 0px;
   padding: 0px;
 }
+
 .feather-k-container-page-title {
-  margin: 20px;
-  /* padding: 4px; */
-  /* background-color: #eee; */
+  margin: 48px 24px 24px;
   max-width: fit-content;
-  /* border: 1px dashed green; */
+  border: 1px dashed green;
 }
 .feather-k-container-standard {
-  display: flex;
-  flex-direction: column;
-  margin: 20px ;
-  padding: 8px;
-  /* background-color: #eee; */
-  border: 1px dashed navy;
+  margin: 24px ;
+  padding: 8px 0 8px;
+  /* border: 1px dashed navy; */
 }
 </style>
 <style scoped>
@@ -114,5 +142,8 @@ body {
 }
 .instruction {
   font-size: 1.5em;
+}
+.patient-search {
+  width: 30em;
 }
 </style>
