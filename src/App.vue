2188705dd35d870ceda8d1649e14ee943c1d5ecb
Jeff Anderson <113629825+janderson9@users.noreply.github.com>
2024-10-24 17:38:29 -0400

    feat(layouts): init (#9)

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,9 @@
 <script setup lang="ts">
-import { ref } from "vue";
+import { reactive, ref } from "vue";
 import { Button } from "@progress/kendo-vue-buttons";
 import { ComboBox } from "@progress/kendo-vue-dropdowns";
 import { Dialog, DialogActionsBar } from "@progress/kendo-vue-dialogs";
-import { PanelBar } from "@progress/kendo-vue-layout";
+import { GridLayout, PanelBar, StackLayout } from "@progress/kendo-vue-layout";
 import {
   Notification,
   NotificationGroup,
@@ -19,12 +19,85 @@ import {
   CardTitle,
 } from "@progress/kendo-vue-layout";
 import { TextBox } from "@progress/kendo-vue-inputs";
+
+import Tip from "./components/Tip.vue";
 import "./assets/css/feather-ks.css";
 // import "./assets/css/feather-ks-v18-migration-test.css"
 import "./assets/css/feather-k-override.css";
 
 let dialogIsVisible = ref(false);
 
+// #region Grid
+
+const gridItems = [
+  { row: 1, col: 1, colSpan: 3, content: "content1" },
+  { row: 1, col: 4, colSpan: 3, content: "content2" },
+  { row: 2, col: 2, colSpan: 2, content: "content3" },
+  { row: 2, col: 4, colSpan: 2, content: "content4" },
+  { row: 3, col: 1, colSpan: 3, content: "content5" },
+  { row: 3, col: 4, colSpan: 3, content: "content6" },
+  { row: 2, col: 1, content: "imgLeft" },
+  { row: 2, col: 6, content: "imgRight" },
+];
+const rows = reactive([
+  { height: "30%" },
+  { height: "30%" },
+  { height: "30%" },
+]);
+const cols = reactive([
+  { width: "16%" },
+  { width: "16%" },
+  { width: "16%" },
+  { width: "16%" },
+  { width: "16%" },
+  { width: "16%" },
+]);
+
+const tips = [
+  {
+    tip: 1,
+    title: "What is the 'gap' attribute?",
+    content:
+      "The gap attribute is used to set the gap (in pixels) between the rows and columns of the grid.",
+  },
+  {
+    tip: 2,
+    title: "The 'rows' Attribute",
+    content:
+      "You set the height of the cell by setting the height of the row. The height of the row can be set in pixels or as a percentage of the grid height.",
+  },
+  {
+    tip: 3,
+    title: "The 'cols' Means Columns",
+    content:
+      "You set the width of the cell by setting the width in the cols attribute. The width of the column can be set in pixels or as a percentage of the grid width.",
+  },
+  {
+    tip: 4,
+    title: "The 'items' Attribute",
+    content:
+      "The items attribute is used to define the grid cells. Each cell is defined by the row and column it occupies, the number of columns it spans, and the content id.",
+  },
+  {
+    tip: 5,
+    title: "The 'template' Tag",
+    content:
+      "The template tag is used to define the content of the grid cell. You can define the content of the cell using the template tag.",
+  },
+  {
+    tip: 6,
+    title: "The 'content' Attribute",
+    content:
+      "The content attribute is used to define the content of the grid cell. You can define the content of the cell using the content attribute.",
+  },
+];
+
+// #endregion Grid
+
+// #region StackLayout
+
+
+// #endregion StackLayout
 const toggleDialog = () => {
   dialogIsVisible.value = !dialogIsVisible.value;
 };
@@ -45,6 +118,11 @@ const close = (style: string) => {
 
 <template>
   <main>
+    <p class="instruction">
+      To run storybook enter "npm run storybook:dev" from the terminal inside VS
+      Code.
+    </p>
+
     <NotificationGroup
       :style="{
         right: 0,
@@ -53,7 +131,11 @@ const close = (style: string) => {
         flexWrap: 'wrap-reverse',
       }"
     >
-      <Fade :appear="showSuccess" :transition-enter-duration="3000" :transition-exit-duration="1000">
+      <Fade
+        :appear="showSuccess"
+        :transition-enter-duration="3000"
+        :transition-exit-duration="1000"
+      >
         <Notification
           :type="{ style: 'success', icon: true }"
           :closable="true"
@@ -61,7 +143,11 @@ const close = (style: string) => {
           >This demo was successfully loaded.</Notification
         >
       </Fade>
-      <Fade :appear="showError" :transition-enter-duration="3000" :transition-exit-duration="1000">
+      <Fade
+        :appear="showError"
+        :transition-enter-duration="3000"
+        :transition-exit-duration="1000"
+      >
         <Notification
           :type="{ style: 'error', icon: true }"
           :closable="true"
@@ -71,10 +157,40 @@ const close = (style: string) => {
       </Fade>
     </NotificationGroup>
 
-    <p class="instruction">
-      To run storybook enter "npm run storybook:dev" from the terminal inside VS
-      Code.
-    </p>
+    <StackLayout
+      :gap="16"
+      :orientation="'vertical'"
+      >
+      <div>Content 1</div>
+      <div>Content 2</div>
+      <div>Content 3</div>
+
+
+    </StackLayout>
+
+    <GridLayout
+      :gap="{ rows: 16, cols: 16 }"
+      :rows="rows"
+      :cols="cols"
+      :items="gridItems"
+    >
+      <template #content1><Tip :item="tips[0]"></Tip></template>
+      <template #content2><Tip :item="tips[1]" /></template>
+      <template #content3><Tip :item="tips[2]" /></template>
+      <template #content4><Tip :item="tips[3]" /></template>
+      <template #content5><Tip :item="tips[4]" /></template>
+      <template #content6><Tip :item="tips[5]" /></template>
+      <template #imgLeft>
+        <div class="image-container">
+          <img src="/vite.svg" alt="Vite" />
+        </div>
+      </template>
+      <template #imgRight>
+        <div class="image-container">
+          <img src="/sb.svg" alt="Storybook" />
+        </div>
+      </template>
+    </GridLayout>
 
     <div class="feather-k-page-title"></div>
 
@@ -264,5 +380,20 @@ body {
 }
 .patient-search {
   width: 30em;
+}
+.grid-content {
+  padding: 0.5em;
+  background-color: #444ffe;
+}
+.image-container {
+  width: 100%;
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  img {
+    width: 100%;
+    height: auto;
+    object-fit: contain;
+  }
 }
 </style>
---
+++ src/components/Tip.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <h2>Tip #{{ item.tip }}</h2>
+    <strong>{{ item.title }}</strong>
+    <p>{{ item.content }}</p>
+  </div>
+</template>
+
+<script setup>
+const props = defineProps({
+  item: Object,
+});
+</script>
+
+<style scoped></style>
