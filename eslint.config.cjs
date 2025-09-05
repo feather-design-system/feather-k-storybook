87a3f646fe3e4620745eda48a2f95103f75f757f
Jeff Anderson <janderson@nanthealth.com>
2025-09-05 17:38:46 -0400

    refactor: remove .eslintignore and streamline ESLint config; clean up Tip.vue

--- eslint.config.cjs
+++ eslint.config.cjs
@@ -1,47 +1,60 @@
 // ESLint flat config for Vue 3 + TypeScript + Storybook
-const path = require('path');
+const path = require("path");
+
+// Patterns taken from .eslintignore
+const ignorePatterns = [
+  "node_modules/**",
+  "dist/**",
+  "storybook-static/**",
+  "build/**",
+  "public/**",
+  "*.min.js",
+  "coverage/**",
+  "*.log",
+];
+
 module.exports = [
   {
-    ignores: ['node_modules/**', 'dist/**', 'storybook-static/**', 'build/**', 'public/**'],
+    ignores: ignorePatterns,
     languageOptions: {
-      parser: require('vue-eslint-parser'),
+      parser: require("vue-eslint-parser"),
       parserOptions: {
-        parser: require('@typescript-eslint/parser'),
-        ecmaVersion: 'latest',
-        sourceType: 'module',
-        extraFileExtensions: ['.vue'],
+        parser: require("@typescript-eslint/parser"),
+        ecmaVersion: "latest",
+        sourceType: "module",
+        extraFileExtensions: [".vue"],
       },
       globals: {
-        window: 'readonly',
-        document: 'readonly',
-        navigator: 'readonly',
+        window: "readonly",
+        document: "readonly",
+        navigator: "readonly",
       },
     },
     plugins: {
-      vue: require('eslint-plugin-vue'),
-      '@typescript-eslint': require('@typescript-eslint/eslint-plugin'),
-      storybook: require('eslint-plugin-storybook'),
-      prettier: require('eslint-plugin-prettier'),
+      vue: require("eslint-plugin-vue"),
+      "@typescript-eslint": require("@typescript-eslint/eslint-plugin"),
+      storybook: require("eslint-plugin-storybook"),
+      prettier: require("eslint-plugin-prettier"),
     },
     linterOptions: {
       reportUnusedDisableDirectives: true,
     },
     rules: {
       // Base rules
-      'no-console': ['warn', { allow: ['warn', 'error'] }],
-  // Run Prettier as an ESLint rule and report formatting issues as errors
-  'prettier/prettier': ['error'],
+      "no-console": ["warn", { allow: ["warn", "error"] }],
+      // Run Prettier as an ESLint rule and report formatting issues as errors
+      "prettier/prettier": ["error"],
       // Vue style
-      'vue/html-indent': ['error', 2],
+      "vue/html-indent": ["error", 2],
       // TypeScript
-      '@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
+      "@typescript-eslint/no-unused-vars": ["warn", { argsIgnorePattern: "^_" }],
     },
   },
   {
-    files: ['**/*.stories.*', 'stories/**/*.*'],
+    files: ["**/*.stories.*", "stories/**/*.*"],
     rules: {
       // Story files frequently export helpers/objects that appear unused
-      '@typescript-eslint/no-unused-vars': 'off',
+      "@typescript-eslint/no-unused-vars": "off",
     },
   },
 ];
