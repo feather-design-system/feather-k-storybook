77f544dffb6601be29c085959f94f164fc47abcc
Jeff Anderson <113629825+janderson9@users.noreply.github.com>
2025-09-08 13:40:40 -0400

    Grid stories for Basic Grid and Row Navigation (#12)
    * refactor: remove .eslintignore and streamline ESLint config; clean up Tip.vue
    
    * chore: add @storybook/addon-docs
    
    * feat: add useGridKeyboardNavigation composable and enhance BasicGrid

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,10 +1,12 @@
 import type { StorybookConfig } from "@storybook/vue3-vite";
 
 const config: StorybookConfig = {
+  // stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx|vue)"],
   stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
 
   addons: [
     "@storybook/addon-links",
+    "@storybook/addon-docs",
     "@chromatic-com/storybook",
   ],
 
--- src/components/Tip.vue
+++ src/components/Tip.vue
@@ -7,7 +7,6 @@
 </template>
 
 <script setup lang="ts">
-
 type TipItem = {
   tip?: number;
   title?: string;
